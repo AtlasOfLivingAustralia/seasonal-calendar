00f67d385b937d2486fb735ec4a341c15ea2eb39
Patricia Koh <patricia.koh@csiro.au>
2018-10-04 14:31:45 +1000

    create db before install

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 pushd seasonal-calendar-api
-cp local.ci local.properties
-psql -c 'create database seasonal-calendars;' -U postgres
-chmod +x gradlew
 ./gradlew test
 ./gradlew build
 ./gradlew assemble --refresh-dependencies
