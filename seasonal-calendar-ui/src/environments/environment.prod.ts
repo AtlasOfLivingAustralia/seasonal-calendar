697df8c75819da59f7974883ad0391d239882f14
Patricia Koh <patricia.koh@csiro.au>
2018-10-05 16:44:03 +1000

    Add travis CI

--- seasonal-calendar-ui/src/environments/environment.prod.ts
+++ seasonal-calendar-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://calendars-api-dev.ala.org.au/',
+  api: 'https://calendars-dev.ala.org.au/api/',
   profiles: 'https://profiles-dev.ala.org.au/'
 };
